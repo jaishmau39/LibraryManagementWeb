868077678ff22287e8d64750d93b1a86ba5c99cc
Jaishma Usha <jaishmarajan8@gmail.com>
2022-09-14 20:34:09 -0400

    Docker support.

--- LibraryManagement/obj/Debug/net6.0/LibraryManagement.AssemblyInfo.cs
+++ LibraryManagement/obj/Debug/net6.0/LibraryManagement.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0dec9051-0d13-4af6-8453-a6ec547734ad")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
